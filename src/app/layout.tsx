cf0f7cafdb5f5876ca425b5ec2260f660c6a2f43
ditinagrawal <ditin.agrawal05@gmail.com>
2026-04-10 11:11:11 +0530

    fix script url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           data-chatbot-id="80997bb9-9873-4d40-a7e6-8d7d5df6ff9d"
         ></script> */}
         <script
-          src="http://localhost:3000/chatBot.js"
+          src="https://ploppy.vercel.app/chatBot.js"
           data-chatbot-id="c3b2094e-0739-4ccf-a080-3bbda744c839"
         ></script>
       </head>
